1c2737a15bfd26544bbc8912edbf0a86b8704765
vektor9999 <vektor9999@gmail.com>
2023-04-03 06:45:01 +0200

    Fixed an error in the mo2 launcher

--- Vcc.Nolvus.Launcher/Program.cs
+++ Vcc.Nolvus.Launcher/Program.cs
@@ -12,15 +12,23 @@ namespace Vcc.Nolvus.Launcher
     {
         static void Main(string[] args)
         {
-            Process SKSEProcess = new Process();
+            if (args.Length > 0)
+            {
+                Process SKSEProcess = new Process();
 
-            SKSEProcess.StartInfo.WorkingDirectory = args[0];            
-            SKSEProcess.StartInfo.FileName =  Path.Combine(args[0], "skse64_loader.exe");
-            SKSEProcess.StartInfo.CreateNoWindow = true;
-            SKSEProcess.StartInfo.UseShellExecute = false;
-            SKSEProcess.StartInfo.WindowStyle = ProcessWindowStyle.Hidden;
+                SKSEProcess.StartInfo.WorkingDirectory = args[0];
+                SKSEProcess.StartInfo.FileName = Path.Combine(args[0], "skse64_loader.exe");
+                SKSEProcess.StartInfo.CreateNoWindow = true;
+                SKSEProcess.StartInfo.UseShellExecute = false;
+                SKSEProcess.StartInfo.WindowStyle = ProcessWindowStyle.Hidden;
 
-            SKSEProcess.Start();
+                SKSEProcess.Start();
+            }
+            else
+            {
+                Console.WriteLine("Nolvus can only be launched through Mod Organizer 2. Execute NolvusDashboard.exe, click on play. When Mod Organizer 2 has started, be sure Nolvus is selected in the right drop down list and click on Run.");
+            }
+            
         }
     }
 }
--- Vcc.Nolvus.Package/Properties/Resources.Designer.cs
+++ Vcc.Nolvus.Package/Properties/Resources.Designer.cs
@@ -63,16 +63,6 @@ namespace Vcc.Nolvus.Package.Properties {
         /// <summary>
         ///   Looks up a localized resource of type System.Drawing.Bitmap.
         /// </summary>
-        internal static System.Drawing.Bitmap Loading_NoBG {
-            get {
-                object obj = ResourceManager.GetObject("Loading_NoBG", resourceCulture);
-                return ((System.Drawing.Bitmap)(obj));
-            }
-        }
-        
-        /// <summary>
-        ///   Looks up a localized resource of type System.Drawing.Bitmap.
-        /// </summary>
         internal static System.Drawing.Bitmap mod_def_22 {
             get {
                 object obj = ResourceManager.GetObject("mod_def_22", resourceCulture);
