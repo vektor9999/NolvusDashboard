a60a34f5116276e7fbd5012e06b7f049fb468339
vektor9999 <vektor9999@gmail.com>
2023-04-05 13:49:38 +0200

    Added header into the web client

--- Vcc.Nolvus.Services/Files/Downloaders/BaseFileDownloader.cs
+++ Vcc.Nolvus.Services/Files/Downloaders/BaseFileDownloader.cs
@@ -71,7 +71,9 @@ namespace Vcc.Nolvus.Services.Files
 
             Wcli.Proxy = null;
 
-            Wcli.UseDefaultCredentials = true;            
+            Wcli.Credentials = System.Net.CredentialCache.DefaultCredentials;
+            Wcli.Headers["Accept"] = "*/*";
+            Wcli.Headers["User-Agent"] = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/110.0.0.0 Safari/537.36";
 
             Wcli.DownloadProgressChanged += ProgressChanged;
             Wcli.DownloadFileCompleted += FileCompleted;
