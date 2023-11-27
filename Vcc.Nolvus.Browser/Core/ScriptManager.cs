f01c0c18f58e777a501405b54672a75d6f7c2718
vektor9999 <vektor9999@gmail.com>
2023-11-27 09:35:57 +0100

    Update 3.4.13

--- Vcc.Nolvus.Browser/Core/ScriptManager.cs
+++ Vcc.Nolvus.Browser/Core/ScriptManager.cs
@@ -8,7 +8,13 @@ namespace Vcc.Nolvus.Browser.Core
 {
     public static class ScriptManager
     {
-        public static string ScrollToButton = "(function () { var el = document.getElementById('slowDownloadButton'); if (el) { el.scrollIntoView({ behavior: 'smooth', block: 'end', inline: 'nearest' }); } })();";
+        public static string ScrollToButton          = "(function () { var el = document.getElementById('slowDownloadButton'); if (el) { el.scrollIntoView({ behavior: 'smooth', block: 'end', inline: 'nearest' }); } })();";
+        public static string IsDownloadAvailable     = "(function () { let val = 0; if (document.getElementById('slowDownloadButton') != null) { val = 1 }; return val; })();";
+        public static string IsFileDeleted           = "(function() { let val = 0; if (document.getElementsByClassName('info-content') && document.getElementsByClassName('info-content')[0].innerHTML.indexOf('This file has been removed') !== -1) { val = 1 }; return val; })();";
+        public static string IsModNotFound           = "(function() { let val = 0; if (document.getElementsByClassName('info-content') && document.getElementsByClassName('info-content')[0].innerHTML.indexOf('Not found') !== -1) { val = 1 }; return val; })();";
+        public static string IsLoginNeeded           = "(function() { let val = 0; if (document.getElementsByClassName('replaced-login-link')[0] != null) { val = 1 }; return val; })();";
+        public static string NexusManualDownloadInit = "document.getElementById('slowDownloadButton').style.border = '5px dashed green';";
+        public static string RedirectToLogin         = "document.getElementsByClassName('replaced-login-link')[0].click();";
 
         public static string ReadScript(string FileName)
         {
@@ -16,28 +22,28 @@ namespace Vcc.Nolvus.Browser.Core
         }
 
         public static string GetIsDownloadAvailable()
-        {
-            return ScriptManager.ReadScript(AppDomain.CurrentDomain.BaseDirectory + "\\Scripts\\IsDownloadAvailable.js");
+        {            
+            return ScriptManager.IsDownloadAvailable;
         }
 
         public static string GetIsFileDeleted()
-        {
-            return ScriptManager.ReadScript(AppDomain.CurrentDomain.BaseDirectory + "\\Scripts\\IsFileDeleted.js");
+        {            
+            return ScriptManager.IsFileDeleted;
         }
 
         public static string GetIsModNotFound()
-        {
-            return ScriptManager.ReadScript(AppDomain.CurrentDomain.BaseDirectory + "\\Scripts\\IsModNotFound.js");
+        {            
+            return ScriptManager.IsModNotFound;
         }
 
         public static string GetIsLoginNeeded()
-        {
-            return ScriptManager.ReadScript(AppDomain.CurrentDomain.BaseDirectory + "\\Scripts\\IsLoginNeeded.js");
+        {            
+            return ScriptManager.IsLoginNeeded;
         }
 
         public static string GetNexusManualDownloadInit()
-        {
-            return ScriptManager.ReadScript(AppDomain.CurrentDomain.BaseDirectory + "\\Scripts\\NexusManualDownloadInit.js");
+        {            
+            return ScriptManager.NexusManualDownloadInit;
         }
 
         public static string GetNexusManualDownload()
@@ -46,8 +52,8 @@ namespace Vcc.Nolvus.Browser.Core
         }
 
         public static string GetRedirectToLogin()
-        {
-            return ScriptManager.ReadScript(AppDomain.CurrentDomain.BaseDirectory + "\\Scripts\\RedirectToLogin.js");
+        {            
+            return ScriptManager.RedirectToLogin;
         }
 
         public static string GetHandleVector(string Url)
