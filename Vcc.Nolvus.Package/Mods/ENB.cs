207bb869d7465f988e599cb0de0a698a095fb005
vektor9999 <vektor9999@gmail.com>
2025-07-03 12:00:41 +0200

    Update 3.7.10

--- Vcc.Nolvus.Package/Mods/ENB.cs
+++ Vcc.Nolvus.Package/Mods/ENB.cs
@@ -69,7 +69,7 @@ namespace Vcc.Nolvus.Package.Mods
                 {
                     if (Patcher != null)
                     {
-                        await Patcher.PatchFiles(ServiceSingleton.Instances.WorkingInstance.InstallDir, ServiceSingleton.Instances.WorkingInstance.StockGame, DownloadingProgress, ExtractingProgress, PatchingProgress);
+                        await Patcher.PatchFiles(ServiceSingleton.Instances.WorkingInstance.StockGame, ServiceSingleton.Instances.WorkingInstance.StockGame, DownloadingProgress, ExtractingProgress, PatchingProgress);
                     }
                 }
                 catch (Exception ex)
